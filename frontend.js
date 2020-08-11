73a553b20a4f22b2c73377599ab9578648b7f990
ajaidanial <ajai@geopits.com>
2020-08-11 08:36:16 +0530

    FE.js done

--- frontend.js
+++ frontend.js
@@ -1,7 +1,27 @@
-let fs = require("fs");
+const fs = require("fs");
+const JSEncrypt = require("node-jsencrypt");
 
-// public key
+// public key & private key
 let public_key = fs.readFileSync("keys/public_key.pem", "utf8").toString();
-
-// private key
 let private_key = fs.readFileSync("keys/private_key.pem", "utf8").toString();
+
+// encryption function
+let encrypt = (string_to_encrypt) => {
+    let encrypt = new JSEncrypt();
+    encrypt.setPublicKey(public_key);
+    return encrypt.encrypt(string_to_encrypt);
+};
+
+// decryption function
+let decrypt = (string_to_decrypt) => {
+    let decrypt = new JSEncrypt();
+    decrypt.setPrivateKey(private_key);
+    return decrypt.decrypt(string_to_decrypt);
+};
+
+// dummy test
+let dummy_data = "Hey, I am working";
+let encrypted_data = encrypt(dummy_data);
+let decrypted_data = decrypt(encrypted_data);
+console.log("Encrypted Data: ", encrypted_data);
+console.log("Decrypted Data: ", decrypted_data);
